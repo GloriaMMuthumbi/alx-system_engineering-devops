776434f602a3c8a45ebd3543ee1ed187c8218f86
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-02-02 12:57:59 -0800

    8. Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #ruby script that outputs: [SENDER], [RECIEVER], [FLAGS]
-puts ARGV[0].scan(/\[from:(\+?w+)\] \[to:(\+?\w+)\] \[flag:(-\d:-?\d:-?\d:-?\d:-?\d)\]/).join(",")
+puts ARGV[0].scan(/\[from:(\+?\w+)\] \[to:(\+?\w+)\] \[flags:(-?\d:-?\d:-?\d:-?\d:-?\d)\]/).join(",")
