6084bc5e1819a89a1d8271f8c43d708cb8c935ef
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-04-22 03:37:28 -0700

    3. Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,10 +6,10 @@ import requests
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "userS").json()
+    user = requests.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dupm({
+        json.dump({
             u.get("id"): [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
