e0530dbaa17027becfe334531585a6310d3b1387
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-05-09 06:04:56 -0700

    1. Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,16 +4,16 @@ the titles of first 10 hot posts listed in subreddit."""
 
 import requests
 
-def top(subreddit):
+def top_ten(subreddit):
     """function to query reddit api and prints the titles
     of the first 10 hot posts in subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     try:
-        response =requests.get(url, headers=headers,
+        response = requests.get(url, headers=headers,
                                 allow_redirects=False)
         if response.status_code == 200:
-            children = response.json)_.get('data').get('children')
+            children = response.json().get('data').get('children')
             for i in range(10):
                 print(children[i].get('data').get('title'))
             else:
